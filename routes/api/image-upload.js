e27b2817a02f0e2579feebd134932a02b16a9f78
Michael <michaelhofmann987@gmail.com>
2019-10-08 19:32:00 -0700

    Minor bug fix on image upload

--- routes/api/image-upload.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const imageCtrl = require('../../controllers/file-upload');
-
-const singleUpload = imageCtrl.upload.single('image');
-
-router.post('/image-upload')
